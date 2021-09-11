452db6dc474475fcf61b3ff79863e888938a7ebb
Konstantin Voloshyn <23.03@i.ua>
2021-09-11 14:47:39 +0300

    some changes

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
+const router = require('./routes');
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api', router);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, (req, res) => {
+            console.log(`Server started ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
